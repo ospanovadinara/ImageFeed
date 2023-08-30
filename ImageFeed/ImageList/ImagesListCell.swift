f2b9216193c15a3901f5209a5fe2246ec3487b30
ospanovadinara <ospan.dinara@yandex.ru>
2023-08-30 15:31:29 +0600

    добавлены image, like button, date label, заданы констрейнты

---
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Dinara on 30.08.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = String(describing: ImagesListCell.self)
+}
