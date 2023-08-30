8c27939001082701d6fd8cc82604417940b3c471
ospanovadinara <ospan.dinara@yandex.ru>
2023-08-30 14:41:14 +0600

    добавила таблицу

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ImagesListViewController.swift
 //  ImageFeed
 //
 //  Created by Dinara on 27.08.2023.
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ImagesListViewController: UIViewController {
 
+    @IBOutlet private var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
