ac5ab44b225476afce8391c85be879d748195b78
ospanovadinara <ospan.dinara@yandex.ru>
2023-09-13 17:07:16 +0600

    single imageview controller added

---
+++ ImageFeed/ImageList/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Dinara on 13.09.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
