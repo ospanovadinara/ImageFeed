6289b827948e229eab8100fe2a9dbf816e31af9d
ospanovadinara <ospan.dinara@yandex.ru>
2023-10-11 15:05:21 +0600

    устранение состояния гонки

--- ImageFeed/Auth/Service/OAuth2Service.swift
+++ ImageFeed/Auth/Service/OAuth2Service.swift
@@ -9,38 +9,22 @@ import Foundation
 
 final class OAuth2Service {
     static let shared = OAuth2Service()
+    private let session = URLSession.shared
+    private var task: URLSessionTask?
+    private var lastCode: String?
+
     private init() {}
 
     func fetchAuthToken(_ code: String,
                         completion: @escaping (Result<String, Error>) -> Void) {
-        //Создание URL c использованием URLComponents
-        var urlComponents = URLComponents()
-        urlComponents.scheme = "https"
-        urlComponents.host = "unsplash.com"
-        urlComponents.path = "/oauth/token"
-        
-        guard let url = urlComponents.url else {
-            return
-        }
-        
-        //Создание HTTP-запроса
-        var request = URLRequest(url: url)
-        
-        //Создание HTTP-метода
-        request.httpMethod = "POST"
-        
-        let bodyParameters = [
-            "client_id": AccessKey,
-            "client_secret": SecretKey,
-            "redirect_uri": RedirectURI,
-            "code": code,
-            "grant_type": "authorization_code"
-        ]
-        
-        request.httpBody = try? JSONSerialization.data(withJSONObject: bodyParameters)
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        let session = URLSession.shared
+
+
+        assert(Thread.isMainThread)
+        if lastCode == code { return }
+        task?.cancel()
+        lastCode = code
+
+        let request = makeRequest(code: code)
         let task = session.dataTask(with: request) { data, response, error in
             DispatchQueue.main.async {
                 if let error = error {
@@ -61,6 +45,10 @@ final class OAuth2Service {
                     do {
                         let responseBody = try decoder.decode(OAuthTokenResponseBody.self, from: data)
                         completion(.success(responseBody.accessToken))
+                        self.task = nil
+                        if error != nil {
+                            self.lastCode = nil
+                        }
                     } catch {
                         completion(.failure(error))
                     }
@@ -69,6 +57,38 @@ final class OAuth2Service {
                 }
             }
         }
+        self.task = task
         task.resume()
+    }
+
+    private func makeRequest(code: String) -> URLRequest {
+        //Создание URL c использованием URLComponents
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "unsplash.com"
+        urlComponents.path = "/oauth/token"
+
+        guard let url = urlComponents.url else {
+            fatalError("Failed to create URL")
+        }
+
+        //Создание HTTP-запроса
+        var request = URLRequest(url: url)
+
+        //Создание HTTP-метода
+        request.httpMethod = "POST"
+
+        let bodyParameters = [
+            "client_id": AccessKey,
+            "client_secret": SecretKey,
+            "redirect_uri": RedirectURI,
+            "code": code,
+            "grant_type": "authorization_code"
+        ]
+
+        request.httpBody = try? JSONSerialization.data(withJSONObject: bodyParameters)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        return request
     }
 }
